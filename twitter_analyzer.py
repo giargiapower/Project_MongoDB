c36b5974759375c57cac7c4044d2e0c7f19a05d6
gianni <gianni.molinari@studenti.unimi.it>
2022-08-22 16:33:40 +0200

    preparazione per salvataggio emoji ed emoticons

--- twitter_analyzer.py
+++ twitter_analyzer.py
@@ -18,6 +18,24 @@ def clear_line(line):
     line = line.replace('URL', '')
     return line
 
+def find_sentimento(name):
+    if name.find("anger") != -1:
+        return "anger"
+    elif name.find("anticipation") != -1:
+        return "anticipation"
+    elif name.find("disgust") != -1:
+        return "disgust"
+    elif name.find("fear") != -1:
+        return "fear"
+    elif name.find("joy") != -1:
+        return "joy"
+    elif name.find("sadness") != -1:
+        return "sadness"
+    elif name.find("surprise") != -1:
+        return "surprise"
+    elif name.find("trust") != -1:
+        return "trust"
+
 
 
 
@@ -27,10 +45,14 @@ client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
 mydb = client["db"]
 col_Hash = mydb["hashtags"]
 print("Connection Successful")
+
 #questo counter l'ho messo per vedere a che punto sono con i caricamenti degli hashtags
 counter = 0
+
+sentimento  = ""
 list = glob.glob("C:/Users/giann/Desktop/UNITO/MAADB_lab/Twitter_messaggi/*.txt")
 for file in list : 
+    sentimento = find_sentimento(file)
     with open(file, 'r', encoding='utf-8') as f:
         lines = f.read()
         clear_l = clear_line(lines)
