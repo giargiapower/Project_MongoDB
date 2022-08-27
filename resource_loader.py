c0e4a38feaabd5bcc7f57336a73748fea58c6eb7
gianni <gianni.molinari@studenti.unimi.it>
2022-08-27 17:32:41 +0200

    caricamento risorse su mongoDB completato

--- resource_loader.py
+++ resource_loader.py
@@ -3,6 +3,11 @@ from tabnanny import process_tokens
 import pymongo
 
 
+def estrai_sentimento(file):
+    file = file.split("\\")
+    return file[1].lower()
+
+
 #connessione a MongoDb
 client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
 mydb = client["db"]
@@ -10,7 +15,15 @@ col_Res = mydb["resources"]
 
 #path messaggi twitter , file emoji e file emoticons 
 list = glob.glob("C:/Users/giann/Desktop/UNITO/MAADB_lab/Risorse_lessicali/*/*.txt")
-for i in list :
-    if i.find("emoji") == -1 & i.find("emoticons") == -1 :
-     print(i,"\n")
-
+for file in list :
+    if file.find("emoji") == -1 & file.find("emoticons") == -1 :
+        with open(file, 'r', encoding='utf-8') as f:
+            sentimento = estrai_sentimento(file)
+            lines = f.read()
+            words = lines.split()
+            for w in words:
+                #non vengono caricate le parole con _ come richiesto da requisiti
+                if w.find("_") == -1 :
+                    mydict = { "word": w, "sentiment" : sentimento}
+                    col_Res.insert(mydict)
+            
